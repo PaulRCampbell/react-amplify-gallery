2fbd49896135605167f5b2653ce8d3a0645a7830
Paul Campbell <PAUL@SYNTHESIS.CO.ZA>
2021-09-04 17:38:10 +0100

    add auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
+import "@aws-amplify/ui/dist/style.css";
 import "./App.css";
 import Layout from "./Layout/Layout";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routing from "./Routing.js";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 function App() {
   return (
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import { Box } from "@material-ui/core";
+import { withAuthenticator } from "aws-amplify-react";
+import AuthTheme from "../../Config/AuthTheme";
+import { signUpConfig } from "../../Config/AuthConfig";
+
 class Projects extends Component {
   render() {
     return <Box>Projects</Box>;
   }
 }
-export default Projects;
+export default withAuthenticator(
+  Projects,
+  false,
+  [],
+  null,
+  AuthTheme,
+  signUpConfig
+);
